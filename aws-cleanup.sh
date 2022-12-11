d6105dde6116192035d33a604dc12be6052b8b87
sharpthinker <142230055+sharpthinker@users.noreply.github.com>
2022-12-11 23:53:30 +0100

    test env url improvement

--- aws-cleanup.sh
+++ aws-cleanup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-echo "[aws-cleanup] Cleanup $appname..."
+echo "[aws-cleanup] Cleanup $environment_name..."
 
 # disable AWS CLI pager
 export AWS_PAGER=""
 
-sam delete --no-prompts --stack-name "$appname" --region "$AWS_DEFAULT_REGION"
+sam delete --no-prompts --stack-name "$environment_name" --region "$AWS_DEFAULT_REGION"
